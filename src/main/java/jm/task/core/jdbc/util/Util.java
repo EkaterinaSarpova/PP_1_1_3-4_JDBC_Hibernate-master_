5c83122c69f9cbc34143833cbf1aa571214b4eec
EkaterinaSarpova <gom04@mail.ru>
2024-04-09 13:37:59 +0500

    реализация задачи с помощью hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,14 +7,9 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
 
 public class Util {
-    //    private static final String DB_URL = "jdbc:mysql://localhost:3306/users";
-//    private static final String DB_NAME = "root";
-//    private static final String DB_PASSWORD = "root";
+
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getConnection() {
@@ -39,16 +34,6 @@ public class Util {
         }
         return sessionFactory;
     }
-//    public static Connection getConnection () {
-//        Connection conn = null;
-//        try {
-//            conn = DriverManager.getConnection(DB_URL, DB_NAME, DB_PASSWORD);
-//            System.out.println("Соединение с БД установлено");
-//        } catch (SQLException e) {
-//            System.out.println("Не удалось подключиться к БД");
-//        }
-//        return conn;
-//    }
 
 }
 
